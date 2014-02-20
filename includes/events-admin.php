30b8abbe600bb482d49a2dabacda2e3c0385a688
IgniteWoo <IgniteWoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-20 16:37:53 +0000

    fixed conflict with WooCommerce JS
    git-svn-id: https://plugins.svn.wordpress.org/event-calendar-ticketing/trunk@861768 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/events-admin.php
+++ includes/events-admin.php
@@ -282,6 +282,8 @@ class IgniteWoo_Events_Admin {
 
 		$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
+		//if ( 'product' == $typenow ) 
+		//	return;
 
 		if ( !$post
 			&& !in_array( $typenow, array( 'ignitewoo_event', 'event_organizer', 'event_venue', 'event_speaker', 'event_sponsor' ) ) 
@@ -291,22 +293,25 @@ class IgniteWoo_Events_Admin {
 			return; 
 
 
-		//if ( 'ignitewoo_event' != $typenow || )
-		//	return;
+		$v = defined( WOOCOMMERCE_VERSION ) ? WOOCOMMERCE_VERSION : null;
 
-		wp_deregister_script( 'jquery-tiptip' );
+		if ( empty( $v ) || version_compare( $v, '2.1', '<' ) )
+			wp_deregister_script( 'jquery-tiptip' );
 		
-		wp_register_script( 'jquery-tiptip', $ignitewoo_events->plugin_url . '/assets/js/jquery-tiptip/jquery.tipTip.min.js', array( 'jquery' ) );
+		if ( empty( $v ) || version_compare( $v, '2.1', '<' ) )
+			wp_register_script( 'jquery-tiptip', $ignitewoo_events->plugin_url . '/assets/js/jquery-tiptip/jquery.tipTip.min.js', array( 'jquery' ) );
+
+		if ( empty( $v ) || version_compare( $v, '2.1', '<' ) )
+			wp_register_style( 'tiptip_css', $ignitewoo_events->plugin_url . '/assets/css/tiptip.css' );
+		
+		if ( empty( $v ) || version_compare( $v, '2.1', '<' ) )
+			wp_deregister_script( 'chosen' );
+		
+		if ( empty( $v ) || version_compare( $v, '2.1', '<' ) )
+			wp_register_script( 'chosen', $ignitewoo_events->plugin_url . '/assets/js/chosen/chosen.jquery'.$suffix.'.js', array('jquery' ), '1.0' );
 
 		wp_enqueue_script( 'jquery-tiptip' );	
-
-		wp_register_style( 'tiptip_css', $ignitewoo_events->plugin_url . '/assets/css/tiptip.css' );
-
 		wp_enqueue_style( 'tiptip_css' );
-		
-		wp_deregister_script( 'chosen' );
-		
-		wp_register_script( 'chosen', $ignitewoo_events->plugin_url . '/assets/js/chosen/chosen.jquery'.$suffix.'.js', array('jquery' ), '1.0' );
 
 		wp_enqueue_script( 'chosen' );
 
@@ -344,7 +349,9 @@ class IgniteWoo_Events_Admin {
 
 
 	function admin_head() { 
-		global $ignitewoo_events;
+		global $ignitewoo_events, $post;
+		
+
 		?>
 
 		<style>
